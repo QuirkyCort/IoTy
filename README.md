bb1a0f7d31b3e9313fd124f3a9d12cfe8c1971a5
Cort <cort@cortscorner.net>
2022-10-11 12:08:38 +0800

    Input example

---
+++ README.md
@@ -0,0 +1,5 @@
+IoTy
+===
+Internet-of-Things made easy
+
+Try it out at https://quirkycort.github.io/IoTy/public/write_files.html
--- examples/input.py
+++ examples/input.py
@@ -1,11 +1,4 @@
 import _ioty_monitor
-import time
-
-for i in range(10):
-    print(i)
-    time.sleep(1)
-
-print('ready')
 
 while True:
     a = input('Enter some text :')
