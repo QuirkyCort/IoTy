8bf6205e048d1fb430426c6de153ba5f40af8adc
Cort <cort@cortscorner.net>
2023-02-18 23:19:39 +0800

    Added i2c blocks

--- public/js/blockly.js
+++ public/js/blockly.js
@@ -50,7 +50,7 @@ var blockly = new function() {
 
   // Load toolbox
   this.loadToolBox = function() {
-    return fetch('toolbox.xml?v=0a791449')
+    return fetch('toolbox.xml?v=cd4a3c2b')
       .then(response => response.text())
       .then(function(response) {
         response = i18n.replace(response);
@@ -80,7 +80,7 @@ var blockly = new function() {
 
   // Load custom blocks
   this.loadCustomBlocks = function() {
-    return fetch('customBlocks.json?v=9b1fdee9')
+    return fetch('customBlocks.json?v=f03ed1aa')
       .then(response => response.text())
       .then(function(response) {
         let json = JSON.parse(i18n.replace(response));
