d17232c76115ad0fa7778a49623a202427bc2879
Cort <cort@cortscorner.net>
2023-11-19 19:58:44 +0800

    Changed block type to Bytes

--- public/js/blockly.js
+++ public/js/blockly.js
@@ -90,7 +90,7 @@ var blockly = new function() {
 
   // Load custom blocks
   this.loadCustomBlocks = function() {
-    return fetch('customBlocks.json?v=14489d99')
+    return fetch('customBlocks.json?v=15aee40d')
       .then(response => response.text())
       .then(function(response) {
         let json = JSON.parse(i18n.replace(response));
