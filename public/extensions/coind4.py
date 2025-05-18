1c5638d56cac35b2fae4849e349b0b0e51d99794
Cort <cort@cortscorner.net>
2025-05-18 19:48:13 +0800

    Fixes to coind4

--- public/extensions/coind4.py
+++ public/extensions/coind4.py
@@ -1,6 +1,5 @@
 import struct
 from micropython import const
-import binascii
 
 CMD_HEADER = const(b'\xAA\x55')
 DATA_HEADER = const(b'\xAA\x55')
@@ -23,12 +22,11 @@ class CoinD4:
             for _ in range(360):
                 self.measurements.append(-1)
         else:
-            for _ in range(500):
+            for _ in range(450):
                 if strength:
                     self.measurements.append([0.0, -1, 0])
                 else:
                     self.measurements.append([0.0, -1])
-
 
     def update(self):
         while self.uart.any():
@@ -85,54 +83,51 @@ class CoinD4:
 
     def _parse_frame(self):
         start_angle, end_angle = struct.unpack('<HH', self.buf[4:8])
-        start_angle = (start_angle >> 1) / 64
-        end_angle = (end_angle >> 1) / 64
+        start_angle = start_angle >> 1
+        end_angle = end_angle >> 1
         if self.buf[2] & 1:
             self.speed = (self.buf[2] >> 1) / 10
 
         if start_angle < self._prev_start_angle and self.integer == False:
-            for i in range(self.measurement_ptr, len(self.measurements)):
-                self.measurements[i][0] = 0.0
-                self.measurements[i][1] = -1.0
-                if self.strength:
-                    self.measurements[i][2] = 0
             self.measurement_ptr = 0
         self._prev_start_angle = start_angle
 
         if self.sample_count > 1:
-            angle_step = (end_angle - start_angle) / (self.sample_count - 1)
+            angle_step = (end_angle - start_angle) // (self.sample_count - 1)
         else:
             angle_step = 0
 
         for i in range(self.sample_count):
-            start_index = 10 + i * 3
-            exposure_mode = 0x03 & self.buf[start_index]
+            start_index = HEADER_LENGTH + i * 3
+            # exposure_mode = 0x03 & self.buf[start_index]
             distance = self.buf[start_index+1] >> 2 | self.buf[start_index+2] << 6
             angle = start_angle + i * angle_step
             if self.integer:
-                self.measurements[round(angle) % 360] = distance
+                self.measurements[(angle // 64) % 360] = distance
             else:
-                self.measurements[self.measurement_ptr][0] = angle
+                self.measurements[self.measurement_ptr][0] = angle / 64
                 self.measurements[self.measurement_ptr][1] = distance
                 if self.strength:
                     self.measurements[self.measurement_ptr][2] = self.buf[start_index+2] >> 2 | (0x03 & self.buf[start_index+1]) << 6
                 self.measurement_ptr += 1
 
-        if end_angle > 345:
+        if end_angle > 22080: # 345 * 64
+            for i in range(self.measurement_ptr, len(self.measurements)):
+                self.measurements[i][1] = -1
             return True
         return False
 
     def _checksum_correct(self):
         cs = self.buf[1] ^ self.buf[3] ^ self.buf[5] ^ self.buf[7]
         for i in range(self.sample_count):
-            cs ^= self.buf[10+i*3+2]
+            cs ^= self.buf[12+i*3]
         if cs != self.buf[9]:
             return False
 
         cs = self.buf[0] ^ self.buf[2] ^ self.buf[4] ^ self.buf[6]
         for i in range(self.sample_count):
             cs ^= self.buf[10+i*3]
-            cs ^= self.buf[10+i*3+1]
+            cs ^= self.buf[11+i*3]
         if cs != self.buf[8]:
             return False
 
