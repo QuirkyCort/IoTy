03846bf58c1e914a2b54cc9604b9af8877ce586a
Cort <cort@cortscorner.net>
2025-05-18 20:30:21 +0800

    Optimize coind4

--- public/extensions/coind4.py
+++ public/extensions/coind4.py
@@ -22,38 +22,42 @@ class CoinD4:
             for _ in range(360):
                 self.measurements.append(-1)
         else:
-            for _ in range(450):
+            for _ in range(420):
                 if strength:
                     self.measurements.append([0.0, -1, 0])
                 else:
                     self.measurements.append([0.0, -1])
 
     def update(self):
+        buf = self.buf
+        ptr = self.ptr
         while self.uart.any():
             chars = self.uart.read(100)
 
             for char in chars:
-                if self.ptr == 0:
+                if ptr == 0:
                     if char == DATA_HEADER[0]:
-                        self.buf[0] = char
-                        self.ptr += 1
-                elif self.ptr == 1:
+                        buf[0] = char
+                        ptr += 1
+                elif ptr == 1:
                     if char == DATA_HEADER[1]:
-                        self.buf[1] = char
-                        self.ptr += 1
+                        buf[1] = char
+                        ptr += 1
                     else:
-                        self.ptr = 0
+                        ptr = 0
                 else:
-                    self.buf[self.ptr] = char
-                    self.ptr += 1
-                    if self.ptr == HEADER_LENGTH:
-                        self.sample_count = self.buf[3]
+                    buf[ptr] = char
+                    ptr += 1
+                    if ptr == HEADER_LENGTH:
+                        self.sample_count = buf[3]
                         if self.sample_count > 25 or self.sample_count == 0:
-                            self.ptr = 0
-                    elif self.ptr == HEADER_LENGTH + self.sample_count * 3:
-                        self.ptr = 0
+                            ptr = 0
+                    elif ptr == HEADER_LENGTH + self.sample_count * 3:
+                        ptr = 0
                         if self._checksum_correct():
+                            self.ptr = ptr
                             return self._parse_frame()
+        self.ptr = ptr
         return False
 
     def _send_cmd(self, code):
@@ -82,14 +86,20 @@ class CoinD4:
         return self.speed * 60
 
     def _parse_frame(self):
-        start_angle, end_angle = struct.unpack('<HH', self.buf[4:8])
+        integer = self.integer
+        strength = self.strength
+        buf = self.buf
+        measurements = self.measurements
+        measurement_ptr = self.measurement_ptr
+
+        start_angle, end_angle = struct.unpack('<HH', buf[4:8])
         start_angle = start_angle >> 1
         end_angle = end_angle >> 1
-        if self.buf[2] & 1:
-            self.speed = (self.buf[2] >> 1) / 10
+        if buf[2] & 1:
+            self.speed = (buf[2] >> 1) / 10
 
-        if start_angle < self._prev_start_angle and self.integer == False:
-            self.measurement_ptr = 0
+        if start_angle < self._prev_start_angle and integer == False:
+            measurement_ptr = 0
         self._prev_start_angle = start_angle
 
         if self.sample_count > 1:
@@ -99,36 +109,39 @@ class CoinD4:
 
         for i in range(self.sample_count):
             start_index = HEADER_LENGTH + i * 3
-            # exposure_mode = 0x03 & self.buf[start_index]
-            distance = self.buf[start_index+1] >> 2 | self.buf[start_index+2] << 6
+            # exposure_mode = 0x03 & buf[start_index]
+            distance = buf[start_index+1] >> 2 | buf[start_index+2] << 6
             angle = start_angle + i * angle_step
-            if self.integer:
-                self.measurements[(angle // 64) % 360] = distance
+            if integer:
+                measurements[(angle // 64) % 360] = distance
             else:
-                self.measurements[self.measurement_ptr][0] = angle / 64
-                self.measurements[self.measurement_ptr][1] = distance
-                if self.strength:
-                    self.measurements[self.measurement_ptr][2] = self.buf[start_index+2] >> 2 | (0x03 & self.buf[start_index+1]) << 6
-                self.measurement_ptr += 1
+                measurements[measurement_ptr][0] = angle / 64
+                measurements[measurement_ptr][1] = distance
+                if strength:
+                    measurements[measurement_ptr][2] = buf[start_index+2] >> 2 | (0x03 & buf[start_index+1]) << 6
+                measurement_ptr += 1
+        self.measurement_ptr = measurement_ptr
 
         if end_angle > 22080: # 345 * 64
-            for i in range(self.measurement_ptr, len(self.measurements)):
-                self.measurements[i][1] = -1
+            if not integer:
+                for i in range(measurement_ptr, len(measurements)):
+                    measurements[i][1] = -1
             return True
         return False
 
     def _checksum_correct(self):
-        cs = self.buf[1] ^ self.buf[3] ^ self.buf[5] ^ self.buf[7]
+        buf = self.buf
+        cs = buf[1] ^ buf[3] ^ buf[5] ^ buf[7]
         for i in range(self.sample_count):
-            cs ^= self.buf[12+i*3]
-        if cs != self.buf[9]:
+            cs ^= buf[12+i*3]
+        if cs != buf[9]:
             return False
 
-        cs = self.buf[0] ^ self.buf[2] ^ self.buf[4] ^ self.buf[6]
+        cs = buf[0] ^ buf[2] ^ buf[4] ^ buf[6]
         for i in range(self.sample_count):
-            cs ^= self.buf[10+i*3]
-            cs ^= self.buf[11+i*3]
-        if cs != self.buf[8]:
+            cs ^= buf[10+i*3]
+            cs ^= buf[11+i*3]
+        if cs != buf[8]:
             return False
 
         return True
