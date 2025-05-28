42762575d05a0ae08afab0464868c495e70038a5
Cort <cort@cortscorner.net>
2025-05-28 23:25:42 +0800

    Removed excess blocks

--- public/js/blockly.js
+++ public/js/blockly.js
@@ -90,7 +90,7 @@ var blockly = new function() {
 
   // Load custom blocks
   this.loadCustomBlocks = function() {
-    return fetch('customBlocks.json?v=1347e5d4')
+    return fetch('customBlocks.json?v=bc39bd5b')
       .then(response => response.text())
       .then(function(response) {
         let json = JSON.parse(i18n.replace(response));
